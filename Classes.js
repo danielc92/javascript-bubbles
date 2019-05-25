9a46bcd26116f2c5aa4e64d8f6d7bc05eca4d1e4
Daniel Corcoran <danielc.92@outlook.com>
2019-05-26 09:51:11 +1000

    Added move function

--- Classes.js
+++ Classes.js
@@ -1,5 +1,10 @@
 function setup() {
-    
+    // p5.js function to create a canvas
+    createCanvas(800, 800);
+}
+
+function draw() {
+
 }
 
 class Bubble {
@@ -8,4 +13,16 @@ class Bubble {
     constructor (x, y) {
         this.x = x
     }
+
+    // changes the coordinate for Bubble instance with a random offset in x and y
+    move () {
+        this.x = this.x + random(-10, 10);
+        this.y = this.y + random(-10, 10);
+    }
+
+    show() {
+        
+    }
 }
+
+
