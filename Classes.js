f274a7728c20f2f5872cca8720bef62025010bdb
Daniel Corcoran <danielc.92@outlook.com>
2019-05-26 10:05:11 +1000

    Added radius

--- Classes.js
+++ Classes.js
@@ -2,6 +2,7 @@ function setup() {
     // p5.js function to create a canvas
     createCanvas(800, 800);
     b1 = new Bubble(100, 200);
+    b2 = new Bubble(200, 409);
 }
 
 function draw() {
@@ -11,27 +12,35 @@ function draw() {
     b1.show();
     b1.move();
     b1.show();
+    b2.show();
+    b2.move();
+    b2.show();
+    b2.move();
+    b2.show();
 }
 
 class Bubble {
     
     // Constructor sets properties of instance of Bubble
-    constructor (x, y) {
+    constructor (x, y, r=15) {
         this.x = x;
         this.y = y;
+        this.r = r;
     }
 
     // changes the coordinate for Bubble instance with a random offset in x and y
     move () {
-        this.x = this.x + random(-10, 10);
-        this.y = this.y + random(-10, 10);
+        this.x = this.x + random(-2, 2);
+        this.y = this.y + random(-2, 2);
     }
 
     show() {
         stroke(255);
-        strokeWeight(4);
+        strokeWeight(3);
         noFill();
-        ellipse(this.x, this.y, 15, 15);
+
+        //ellise() takes in x, y, width and height
+        ellipse(this.x, this.y, this.r, this.r);
     }
 }
 
